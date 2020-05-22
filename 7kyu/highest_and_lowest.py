83044112c1ac9bee3856d4d1e1aa9d2e1930f60d
Samiul Islam Ponik <65505655+samiulislamponik@users.noreply.github.com>
2020-05-22 17:15:08 +0600

    Add files via upload

---
+++ 7kyu/highest_and_lowest.py
@@ -0,0 +1,18 @@
+def high_and_low(numbers):
+
+    new_list = list(map(int, numbers.split()))
+    max = new_list[0]
+    for i in range(len(new_list)):
+        if new_list[i] > max:
+            max = new_list[i]
+
+    min = new_list[0]
+    for i in range(len(new_list)):
+        if new_list[i] < min:
+            min = new_list[i]
+
+    output = f'{max} {min}'
+    return output
+
+
+print(high_and_low("1 2 -3 4 5"))
