aaf5d98fab5ac1cfe2085e483afa4f3ac6488840
Samiul Islam Ponik <65505655+samiulislamponik@users.noreply.github.com>
2020-05-22 16:36:21 +0600

    Add files via upload

---
+++ 7kyu/vowel_count.py
@@ -0,0 +1,9 @@
+# Count the vowels into a string.
+
+def getCount(inputStr):
+    count = 0
+    count = inputStr.count("a") + inputStr.count("e") + inputStr.count("i") + inputStr.count("o") + inputStr.count("u")
+    return count
+
+
+print(getCount("abcdeeiuf"))
