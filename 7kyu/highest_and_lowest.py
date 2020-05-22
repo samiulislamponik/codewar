4f066b3d8018e9e35b3ff861e6d09df9df1290de
Samiul Islam Ponik <65505655+samiulislamponik@users.noreply.github.com>
2020-05-22 17:17:12 +0600

    Update highest_and_lowest.py

--- 7kyu/highest_and_lowest.py
+++ 7kyu/highest_and_lowest.py
@@ -1,3 +1,6 @@
+# In this assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+
 def high_and_low(numbers):
 
     new_list = list(map(int, numbers.split()))
@@ -16,3 +19,5 @@ def high_and_low(numbers):
 
 
 print(high_and_low("1 2 -3 4 5"))
+
+
