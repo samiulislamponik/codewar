99892317f4754596ca26048c76fb9c3421af7826
Samiul Islam Ponik <65505655+samiulislamponik@users.noreply.github.com>
2020-05-24 00:58:48 +0600

    Add files via upload

---
+++ 7kyu/narcissistic.py
@@ -0,0 +1,19 @@
+def narcissistic(value):
+    if value == int(value):
+        new_str = str(value)
+        l = len(new_str)
+        sum = 0
+        for i in new_str:
+            sum += int(i)**l
+
+        if value == sum:
+            return True
+        else:
+            return False
+
+    else:
+
+        return "For text strings or other invalid inputs is not required, only valid integers will be passed into the function."
+
+
+print(narcissistic("32"))
