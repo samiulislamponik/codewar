65253dd7268b46adb77e0a14374f5fe2d42cfdff
Samiul Islam Ponik <65505655+samiulislamponik@users.noreply.github.com>
2020-05-23 22:24:47 +0600

    Add files via upload

---
+++ 7kyu/anagrams.py
@@ -0,0 +1,20 @@
+def anagrams(word, words):
+
+    check_list = sorted(list((word)))
+    new_list = list(map(list,words))
+
+    ultimate_list = []
+    for i in new_list:
+
+        if check_list == sorted(i):
+            s = "".join(i)
+            ultimate_list.append(s)
+
+    if not ultimate_list:
+        return []
+    else:
+        return ultimate_list
+
+
+
+print(anagrams('abba', ['dsff', 'abcd', 'a', 'dada']))
