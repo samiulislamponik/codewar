4e2a0472f866495acef294519102481eeba7d53a
Samiul Islam Ponik <65505655+samiulislamponik@users.noreply.github.com>
2020-05-27 15:23:58 +0600

    Add files via upload

---
+++ 6kyu/Hashtag_generator.py
@@ -0,0 +1,17 @@
+def generate_hashtag(s):
+    if not s:
+        return False
+    else:
+        x = s.split()
+        new_list = []
+        for i in x:
+            new_list.append(i.capitalize())
+
+        new_string = '#' + "".join(new_list)
+        if len(new_string) > 140:
+            return False
+        else:
+            return new_string
+
+
+print(generate_hashtag(''))
