2280b391eaee8f616de579c24c9fe87d92e5ed7b
Samiul Islam Ponik <65505655+samiulislamponik@users.noreply.github.com>
2020-05-28 22:17:40 +0600

    Update Highest_scoring_word.py

--- 6kyu/Highest_scoring_word.py
+++ 6kyu/Highest_scoring_word.py
@@ -1,7 +1,7 @@
 
 def high(x):
     new_list = x.split()
-    new_list.append("0")
+    new_list.append("0")  # For extra iteration.
     char_list = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26}
     arr = []
     j = 0
@@ -25,3 +25,4 @@ def high(x):
 
 
 print(high('man i need a taxi up to ubud'))
+
