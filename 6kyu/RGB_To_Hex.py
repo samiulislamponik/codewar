23c1eac209031c1b010a8918ccf0edf5515f6500
Samiul Islam Ponik <65505655+samiulislamponik@users.noreply.github.com>
2020-05-29 09:35:40 +0600

    Add files via upload

---
+++ 6kyu/RGB_To_Hex.py
@@ -0,0 +1,20 @@
+def rgb(r, g, b):
+    if r < 0:
+        r = 0
+    elif r > 255:
+        r = 255
+    if g < 0:
+        g = 0
+    elif g > 255:
+        g = 255
+    if b < 0:
+        b = 0
+    elif b > 255:
+        b = 255
+
+    value = hex(r)[2:].zfill(2) + hex(g)[2:].zfill(2) + hex(b)[2:].zfill(2)
+
+    return value.upper()
+
+
+print(rgb(-20,275,125))
