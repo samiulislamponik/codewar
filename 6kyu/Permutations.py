ed45da332e84a1f891b8d0df4c0d52664380d54e
Samiul Islam Ponik <65505655+samiulislamponik@users.noreply.github.com>
2020-06-03 18:54:12 +0600

    Add files via upload

---
+++ 6kyu/Permutations.py
@@ -0,0 +1,26 @@
+def permutations(string):
+
+    from itertools import permutations
+
+    combination = permutations(string)
+    li = []
+    for i in combination:
+
+        s = "".join(i)
+        li.append(s)
+    new_li = set(li)
+    return list(new_li)
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(permutations('aabb'))
